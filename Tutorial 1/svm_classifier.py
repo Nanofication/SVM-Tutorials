31b81d608127e750ba6fcb38edb9788bf0535493
Nanofication <nansquared@gmail.com>
2017-08-16 09:58:35 -0400

    Tweaked gamma

--- Tutorial 1/svm_classifier.py
+++ Tutorial 1/svm_classifier.py
@@ -8,12 +8,17 @@ from sklearn import svm # I <3 SVM
 
 digits = datasets.load_digits()
 
-# Gamma matters
-clf = svm.SVC(gamma=0.001, C=100)
+# Gamma matters (Isn't this learning rate?)
+clf = svm.SVC(gamma=0.0001, C=100)
+# What gamma does is to take steps to the global minima
+# Gradient descent. You're trying to get down a mountain.
+# What is the proper number of leap size to get to your answer as quickly as efficiently as possible
 
-x, y = digits.data[:-1],digits.target[:-1] # This is data and targets all the way up to last number
-clf.fit(x,y)
+x, y = digits.data[:-10],digits.target[:-10] # This is data and targets all the way up to last number
+clf.fit(x,y) # Fits a line to the numbers plotted on the graph
+# Low number of variables, you can plot it. But if you see squiggly lines you're overfitting
 
-print "Prediction: ", clf.predict(digits.data[-1])
+print "Prediction: ", clf.predict(digits.data[-2])
 
-plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.show()
